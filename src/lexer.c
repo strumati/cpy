3943a7660f4654d0e9c921343122ed527093159e
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-04-25 20:31:36 +0200

    basic test lexer build

--- src/lexer.c
+++ src/lexer.c
@@ -1,18 +1,45 @@
-//pseudocode for now
+//stand alone test lexer
 
-char[][][] *lexer(char[][] *code) {
-    foreach(code=>c,lexer_str(c))
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char **lexer_str(char *code) {
+    char **tokens = malloc(4 * sizeof(char *));
+    tokens[0] = strdup("VAR:x");
+    tokens[1] = strdup("OPERATOR:=");
+    tokens[2] = strdup("INT:2");
+    tokens[3] = NULL;
+    return tokens;
 }
 
-char[][] *lexer_str(char[] *code) {
-    type codetype = get_type(code);
-    if(codetype == type_var) {
-
+char ***lexer(char **code) {
+    int i = 0;
+    
+    while (code[i] != NULL) i++;
+    
+    char ***result = malloc((i + 1) * sizeof(char **));
+    
+    for (int j = 0; j < i; j++) {
+        result[j] = lexer_str(code[j]);
     }
-    if(codetype == type_def) {
+    result[i] = NULL;
+    return result;
+}
 
+int main() {
+    char line1[] = "x = 2";
+    char line2[] = "print(x)";
+    char *lines[] = {line1,line2,NULL};
+    char ***result = lexer(lines);
+    
+    for (int i = 0; result[i] != NULL; i++) {
+        printf("Group [%d]:\n", i);
+        
+        for (int j = 0; result[i][j] != NULL; j++) {
+            printf("  Token [%d]: \"%s\"\n", j, result[i][j]);
+        }
     }
-    if(codetype == type_run) {
-
-    }
+    
+    return 0;
 }
---
+++ src/lexer.mockup
@@ -0,0 +1,18 @@
+//pseudocode for now
+
+char[][][] *lexer(char[][] *code) {
+    foreach(code=>c,lexer_str(c))
+}
+
+char[][] *lexer_str(char[] *code) {
+    type codetype = get_type(code);
+    if(codetype == type_var) {
+
+    }
+    if(codetype == type_def) {
+
+    }
+    if(codetype == type_run) {
+
+    }
+}
